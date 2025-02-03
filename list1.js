6ea5daf636d53845b219a42cee252042410c628a
rasithm <mohamedrasith134@gmail.com>
2025-02-04 00:46:05 +0530

    filter only

--- list1.js
+++ list1.js
@@ -2,6 +2,18 @@ let showMenuBar = document.getElementById('c-3-show');
 let showMenuBar1 = document.getElementById('c-3-show-1');
 let showMenuBar2 = document.getElementById('c-3-show-2');
 
+let PostDate = document.getElementById('Post-Date')
+
+PostDate.addEventListener('click' ,function(){
+    let PostDateClick = document.getElementById('post-date-click');
+    if(PostDateClick.style.display === 'none' || PostDateClick.style.display === ''){
+        PostDateClick.style.display = 'flex';
+    }else{
+        PostDateClick.style.display = 'none';
+    }
+})
+
+
 showMenuBar.addEventListener('click', function() {
     let c3Post = document.getElementById('c-3-post');
     let c3Vacancy = document.getElementById('c-3-vacancy');
@@ -40,3 +52,21 @@ showMenuBar2.addEventListener('click', function() {
         c3Vacancy2.style.display = "none";
     }
 });
+
+const filterDiv = document.getElementById('m-filter-div');
+
+filterDiv.addEventListener('click' , function(){
+    mobileView();
+})
+
+const mobileView = () => {
+    if(window.innerWidth >= 320 && window.innerWidth <= 999){
+        let overLay = document.getElementById('overlay')
+        overLay.style.display = 'flex';
+        
+    }
+}
+
+
+window.addEventListener('resize' , mobileView)
+
